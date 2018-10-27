886cefc23570e42141c0640fbf36817239c57c27
Jon Borgonia <jon@gomagames.com>
2018-10-26 22:05:59 -0700

    fixes all the things that were broken
    - on client and server
    - positioning fixed
    - chat fixed

--- client-handler.js
+++ client-handler.js
@@ -4,13 +4,21 @@ const OP = require('./public/js/OP');
 const players = new Map();
 
 const clientConnected = client => {
-  client.username = null;
+  client.username = null; // unregistered
   client.sendOp = sendOp;
   client.receiveOp = receiveOp;
+  client.register = register;
 
   client.on('message', receiveMessage.bind(client));
   client.on('close', disconnect.bind(client));
 };
+
+const register = function({ username, avatarId }){
+  this.username = username;
+  this.avatarId = avatarId;
+  this.position = { x: 0, y: 0 };
+  players.set(this.username, this);
+}
 
 // handles errors
 const sendOp = function(op, payload){
@@ -31,41 +39,47 @@ const receiveOp = function(msg){
       this.sendOp(OP.ERROR, { error });
       break;
     case OP.CHAT:
+      let { message } = msg.payload;
       players.forEach((player, playerUsername) => {
-        if(playerUsername === this.username) return;
-
-        let message = msg.payload.message;
         player.sendOp(OP.CHAT, { username : this.username, message });
       });
       break;
     case OP.ENTER_WORLD:
-      // give current player initial state of the game, no coords
-      let playerUsernamesAvatars = [];
-      for (let { username, avatarId } of players.values()) {
-        playerUsernamesAvatars.push({username, avatarId});
-      }
-      this.sendOp(OP.ENTER_WORLD_ACK, playerUsernamesAvatars);
+      // give current player initial state of the game
+      this.sendOp(OP.ENTER_WORLD_ACK,
+        [ ...players.values() ].map(({
+          username,
+          avatarId,
+          position
+        }) => ({
+          username,
+          avatarId,
+          position
+        }))
+      );
 
-      // broadcast new player
+      // broadcast new player to all existing players
       players.forEach( (player, playerUsername, map) => {
-        if(player !== this){
-          player.sendOp(OP.NEW_PLAYER, { username : this.username, avatarId : this.avatarId });
-        }
+        if(player === this) return;
+        player.sendOp(OP.NEW_PLAYER, {
+          username : this.username,
+          avatarId : this.avatarId,
+          position : this.position
+        });
       });
       break;
     case OP.MOVE_TO:
       let position = msg.payload;
+      this.position = position;
       players.forEach( (player, playerUsername, map) => {
-        if(player !== this){
-          player.sendOp(OP.MOVE_TO, { username: player.username, position });
-        }
+        if(player === this) return;
+        player.sendOp(OP.MOVE_TO, { username: this.username, position });
       });
       break;
     case OP.STOP_MOVING:
       players.forEach( (player) => {
-        if(player !== this){
-          player.sendOp(OP.STOP_MOVING, msg.payload);
-        }
+        if(player === this) return
+        player.sendOp(OP.STOP_MOVING, msg.payload);
       });
       break;
     default:
@@ -96,9 +110,9 @@ const receiveMessage = function(message){
         this.sendOp(OP.ERROR, { error });
       } else {
         // username is available, register the player
-        this.username = msg.payload.username;
-        players.set(this.username, this);
+        this.register(msg.payload);
         this.sendOp(OP.REGISTERACK);
+        console.info(`Client username:'${this.username}' has joined.`);
       }
     } else {
       const error = `You are not registered yet. Register with OP:REGISTER first.`;
@@ -115,8 +129,8 @@ const disconnect = function(){
     if( players.has(this.username) ){
       players.delete(this.username);
     }
-  }
-  console.info(`Client username:'${this.username}' has disconnected.`);
+    console.info(`Client username:'${this.username}' has disconnected.`);
+  } else console.debug(`Client <anonymous> has disconnected.`);
 }
 
 module.exports = {
